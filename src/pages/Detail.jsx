0ae3d05f959369352b5e1effdc076553f0fd2c50
jinny0526 <ertmiu96@gmail.com>
2023-08-06 12:39:59 +0900

    detail 수정은 해봤는데... 여전히 안됨

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -8,7 +8,17 @@ import api from "../axios/api";
 export default function Detail() {
   const navigate = useNavigate();
   const { id } = useParams();
+
   const user = useSelector((state) => state.user);
+  const 로그인확인 = () => {
+    if (!user.email) return false;
+    return true;
+  };
+
+  const 작성자확인 = (author) => {
+    if (user.email !== author) return false;
+    return true;
+  };
   const queryClient = new useQueryClient();
 
   //main 부분과 대부분 일치해서 더 쓸만한 내용은 없는데
@@ -34,6 +44,21 @@ export default function Detail() {
       },
     }
   );
+  //
+  const deleteTodo = async () => {
+    if (!로그인확인()) {
+      return alert("로그인 후 사용할 수 있습니다.");
+    }
+    if (!작성자확인(todo.author)) {
+      return alert("작성자가 일치하지 않습니다.");
+    }
+    const result = window.confirm("정말로 삭제하시겠습니까?");
+    if (result) {
+      await mutation.mutateAsync(todo.id);
+      navigate("/");
+    }
+  };
+
   if (isLoading) {
     return <div>데이터 가져오는 중...</div>;
   }
@@ -42,101 +67,75 @@ export default function Detail() {
     return <div>{error.message}</div>;
   }
 
-  const 로그인확인 = () => {
-    if (!user.email) return false;
-    return true;
-  };
-
-  const 작성자확인 = (author) => {
-    if (user.email !== author) return false;
-    return true;
-  };
-
   return (
     <>
       <Header />
       <Container>
-        <h1
-          style={{
-            border: "1px solid lightgray",
-            borderRadius: "12px",
-            padding: "12px",
-          }}
-        >
-          {todo?.title}
-          {/* 여기가 없을 수도 있다. */}
-        </h1>
-        <div
-          style={{
-            height: "400px",
-            border: "1px solid lightgray",
-            borderRadius: "12px",
-            padding: "12px",
-          }}
-        >
-          {todo?.content}
-        </div>
-        <div
-          style={{
-            marginTop: "12px",
-            display: "flex",
-            justifyContent: "end",
-          }}
-        >
-          <button
-            onClick={() => {
-              if (!로그인확인()) {
-                return alert("로그인 후 사용할 수 있습니다.");
-              }
-
-              if (!작성자확인(todo.author)) {
-                return alert("작성자가 일치하지 않습니다.");
-              }
-              navigate(`/edit/${todo.id}`, {
-                state: {
-                  todo,
-                },
-              });
-            }}
-            style={{
-              border: "none",
-              padding: "8px",
-              borderRadius: "6px",
-              backgroundColor: "orange",
-              color: "white",
-              cursor: "pointer",
-              marginRight: "6px",
-            }}
-          >
-            수정
-          </button>
-          <button
-            onClick={() => {
-              if (!로그인확인()) {
-                return alert("로그인 후 사용할 수 있습니다.");
-              }
-
-              if (!작성자확인(todo.author)) {
-                return alert("작성자가 일치하지 않습니다.");
-              }
-              const result = window.confirm("정말로 삭제하시겠습니까?");
-              if (result) {
-                mutation.mutate(todo.id);
-                navigate("/");
-              }
-            }}
-            style={{
-              border: "none",
-              padding: "8px",
-              borderRadius: "6px",
-              backgroundColor: "red",
-              color: "white",
-              cursor: "pointer",
-            }}
-          >
-            삭제
-          </button>
-        </div>
+        {로그인확인() && 작성자확인(todo.author) && (
+          <>
+            <h1
+              style={{
+                border: "1px solid lightgray",
+                borderRadius: "12px",
+                padding: "12px",
+              }}
+            >
+              {todo?.title}
+              {/* 여기가 없을 수도 있다. */}
+            </h1>
+            <div
+              style={{
+                height: "400px",
+                border: "1px solid lightgray",
+                borderRadius: "12px",
+                padding: "12px",
+              }}
+            >
+              {todo?.content}
+            </div>
+            <div
+              style={{
+                marginTop: "12px",
+                display: "flex",
+                justifyContent: "end",
+              }}
+            >
+              <button
+                onClick={() => {
+                  navigate(`/edit/${todo.id}`, {
+                    state: {
+                      todo,
+                    },
+                  });
+                }}
+                style={{
+                  border: "none",
+                  padding: "8px",
+                  borderRadius: "6px",
+                  backgroundColor: "orange",
+                  color: "white",
+                  cursor: "pointer",
+                  marginRight: "6px",
+                }}
+              >
+                수정
+              </button>
+              <button
+                onClick={deleteTodo}
+                style={{
+                  border: "none",
+                  padding: "8px",
+                  borderRadius: "6px",
+                  backgroundColor: "red",
+                  color: "white",
+                  cursor: "pointer",
+                }}
+              >
+                삭제
+              </button>
+            </div>
+          </>
+        )}
       </Container>
     </>
   );
